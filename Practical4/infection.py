639ba40ce095cf4b5a1dc100d7fbd20bea5210a5
mmmooollll <tong.25@intl.zju.edu.cn>
2026-03-11 15:10:32 +0800

    Update infection.py

--- Practical4/infection.py
+++ Practical4/infection.py
@@ -6,5 +6,5 @@ print(f"day {day}，infected people：{infected} ")
 while infected < total_people:
     infected = infected + infected * growth_rate
     day += 1 
-    print(f"day {day}，infected people：{infected:.1f} ")
+    print(f"day {day}，infected people：{infected:.0f} ")
 print(f"\n all {total_people} people are infected， {day} days are taken")
