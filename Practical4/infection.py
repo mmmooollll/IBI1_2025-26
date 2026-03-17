f32f2d3e20df3124b11b54e28f766851b497eafa
mol <mol@moldeMacBook-Air.local>
2026-03-17 15:56:54 +0800

    Add Practical5: gene expression, heart rate, population analysis

--- Practical4/infection.py
+++ Practical4/infection.py
@@ -1,19 +1,10 @@
-
-total_people = 91  # 班级总人数
+total_people = 91  
 infected = 5       
-growth_rate = 0.4  # 24小时感染增长率（40%）
-day = 1            # 初始天数
-
-# 输出第1天数据
-print(f"第 {day} 天，感染人数：{infected} 人")
-
-# 循环计算每日感染人数
+growth_rate = 0.4 
+day = 1          
+print(f"day {day}，infected people：{infected} ")
 while infected < total_people:
-    # 计算次日感染人数
     infected = infected + infected * growth_rate
-    day += 1  # 天数+1
-    # 输出当天数据（保留1位小数，更直观）
-    print(f"第 {day} 天，感染人数：{infected:.1f} 人")
-
-# 循环结束，输出总天数
-print(f"\n全部 {total_people} 人被感染，总共用了 {day} 天")
+    day += 1 
+    print(f"day {day}，infected people：{infected:.0f} ")
+print(f"\n all {total_people} people are infected， {day} days are taken")
